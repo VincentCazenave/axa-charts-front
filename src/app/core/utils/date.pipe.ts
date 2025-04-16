29df6b8a0746488ddf14bf981d2add39d7a9c673
VincentCazenave <cazenave@et.esiea.fr>
2025-04-16 17:26:33 +0200

    create date pipe

---
+++ src/app/core/utils/date.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'formatDate'
+})
+export class FormatDatePipe implements PipeTransform {
+  transform(value: Date | string | number): string {
+    if (!value) return '';
+    
+    const date = new Date(value);
+    return date.toLocaleString('fr-FR', {
+      day: '2-digit',
+      month: 'short',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+    });
+  }
+}
